adf5ecd01b82a097ab95a9a0f6f2ea9b404adefe
Franck Lebourgeois <franck.lebourgeois@gmail.com>
2022-09-29 08:44:00 +0200

    add data via form, projects + services display

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use App\Models\Project;
+use App\Models\Service;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(){
+
+        $services = Service::all()->take(4);
+        $projects = Project::all()->take(6);
+        return view('page',['services' => $services, 'projects' => $projects]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required|min:5|max:50',
+            'email' => 'email:rfc,dns|required',
+            'phone' => 'nullable|size:10',
+            'message' => 'max:255'
+        ]);
+
+        $customer = new Customer;
+        $customer->name = $request->input('name');
+        $customer->email = $request->input('email');
+        $customer->phone = $request->input('phone');
+        $customer->message = $request->input('message');
+        $customer->save();
+        return Redirect::route('home');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
